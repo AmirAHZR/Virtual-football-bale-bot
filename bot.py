b1bc50709e1c89258d862cdc4b9e14e826dd9942
AmirAHZR <amirhosseinzareiifar@gmail.com>
2026-07-30 22:01:51 +0330

    functionize bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,17 @@ from main import clubs
 from balethon import Client
 from balethon.conditions import private, text
 
+def show_team(text):
+    t = ""
+    for position in ["دروازه‌بان", "مدافع", "هافبک", "مهاجم",'وینگر']:
+        t += f"{position}"
+        ls = [clubs[text].get_players_by_position(position)][0]
+        res = " \\\ ".join(ls)
+        t += f"\n"+res+"\n"
+        
+    t+="اگه می خوای تو ساخت بازی فوتبال مجازی با بات بله کمک کنی تو https://github.com/AmirAHZR/Virtual-football-bale-bot/tree/main در خدمتم"
+    return t
+
 bot = Client("937461250:tRBJJH3meQL5KZkt69ORgNCcVQ7griV7cm8")
 
 
@@ -9,15 +20,8 @@ bot = Client("937461250:tRBJJH3meQL5KZkt69ORgNCcVQ7griV7cm8")
 async def echo(message):
     try:
         if clubs[message.text]:
-            t = ""
-            for position in ["دروازه‌بان", "مدافع", "هافبک", "مهاجم",'وینگر']:
-                t += f"{position}"
-                ls = [clubs[message.text].get_players_by_position(position)][0]
-                res = " \\\ ".join(ls)
-                t += f"\n"+res+"\n"
-                
-            t+="اگه می خوای تو ساخت بازی فوتبال مجازی با بات بله کمک کنی تو https://github.com/AmirAHZR/Virtual-football-bale-bot/tree/main در خدمتم"
-            await message.reply(t)
+            
+            await message.reply(show_team(message.text))
             
     except:
         pass
