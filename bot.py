3074ed1aeca9b1ef8b66e6217d986872eff97c0c
AmirAHZR <amirhosseinzareiifar@gmail.com>
2026-07-30 16:48:48 +0330

    start project

---
+++ bot.py
@@ -0,0 +1,27 @@
+from main import clubs
+from balethon import Client
+from balethon.conditions import private, text
+
+bot = Client("937461250:tRBJJH3meQL5KZkt69ORgNCcVQ7griV7cm8")
+
+
+@bot.on_message(private & text)
+async def echo(message):
+    try:
+        if clubs[message.text]:
+            t = ""
+            for position in ["دروازه‌بان", "مدافع", "هافبک", "مهاجم",'وینگر']:
+                t += f"{position}"
+                ls = [clubs[message.text].get_players_by_position(position)][0]
+                res = " \\\ ".join(ls)
+                t += f"\n"+res+"\n"
+                
+            t+="اگه می خوای تو ساخت بازی فوتبال مجازی با بات بله کمک کنی تو @Amir_AHZR در خدمتم"
+            await message.reply(t)
+            
+    except:
+        pass
+
+
+bot.run()
+
---
+++ main.py
@@ -0,0 +1,48 @@
+import json
+from typing import List
+
+
+class Player:
+    def __init__(self, name: str, post: str):
+        self.name = name
+        self.post = post
+
+    def __repr__(self):
+        return self.name
+
+
+class Club:
+    def __init__(self, name: str, players: List[Player] = None):
+        self.name = name
+        self.players = players if players else []
+
+    def get_players_by_position(self, post: str) -> List[str]:
+        
+        return [p.name for p in self.players if post.lower() in p.post.lower()]
+
+    def show_squad(self):
+        print(f"\n{'='*40}")
+        print(f"  {self.name}")
+        print(f"{'='*40}")
+        for player in self.players:
+            print(player.name)
+
+    def __repr__(self):
+        return f"Club({self.name}, {len(self.players)} players)"
+
+
+def load_clubs(filename) -> dict[str, Club]:
+    with open(filename, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    clubs = {}
+    for club_name, players_data in data.items():
+        players = [
+            Player(name=p["نام"], post=p["پست"])
+            for p in players_data
+        ]
+        clubs[club_name] = Club(club_name, players)
+
+    return clubs
+
+clubs = load_clubs("top20_squads_persian.json")
